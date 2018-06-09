161c9fcb1863fdbb7fb006f64df7ca6f76a5d1c9
danielrthomas <danielrthomas1@gmail.com>
2018-06-09 13:34:04 -0500

    a lot of flask progress

--- world_cup_flask.py
+++ world_cup_flask.py
@@ -3,13 +3,15 @@ from flask import Flask,render_template_string,request,render_template
 app = Flask(__name__)                         
 
 @app.route('/')
-def input_handle():
+def main():
     handle = request.args.get('handle')
     if handle == None or handle == '':
         #get username
-        print 'here'
         return render_template('input_handle.html')
     else:
-        pass
         #we have username, now compute the scores and return
+        ranking = [[25,'Country','realDonaldTrump',['nasty','woman','im','with','her']]]
+        return render_template('result.html',rank=ranking)
+
+    
 app.run()
