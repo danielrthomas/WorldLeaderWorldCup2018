4ff0562c4ea5bc1128680ddecdee3e047fd6d5a5
Noopur <noopurgpt25@gmail.com>
2018-06-10 12:05:13 -0500

    similar docs funcion

--- scoring_leaders.py
+++ scoring_leaders.py
@@ -32,21 +32,14 @@ import json
 #     return res
 
 
-def get_terms_weights(content):
-    leaders_content = []
-    tfidf_vectorizer = TfidfVectorizer (max_df=0.85, min_df=2, stop_words='english')
-    tfidf = tfidf_vectorizer.fit_transform (leaders_content)
-    tfidf_feature_names = tfidf_vectorizer.get_feature_names ()
-
 
 def compute_idf():
-    tfidf_vect = TfidfVectorizer()
+    tfidf_vect = TfidfVectorizer(stop_words="english")
     for name in screen_names:
         with open ("JSONs/" + name.lower (), "r") as f:
             for line in f:
                 data = json.loads(line)
-                # print(data[1]["content"])
-                # content.append(data[1]["content"])
+
             tfidf = tfidf_vect.fit([data[1]['content']])
             pickle.dump(tfidf, open('pickle/' + name.lower(), "wb"))
 
@@ -64,62 +57,102 @@ def compute_idf():
 
 # tfidf_vect = pickle.load("some_file_name")
 
-def score_user(screen_name):
+def leader_user_score(screen_name):
     user_tweets = translateTweetsJson(screen_name, False, False, False, 25)[1]["content"]
-    words = user_tweets.split(" ")
-    #words = words[:500]
-    print("NUM WORDS = " + str(len(words)))
-    print(user_tweets)
-    word_scores = []
-    scores = {}
-    minimum = float("inf")
-    maximum = float("inf")
-    sum = float(0)
+    similar_scores = {}
+    vectorizer = TfidfVectorizer (stop_words='english')
+
     for name in screen_names:
-        tfidf_vect = pickle.load (open("pickle/"+name.lower(), "rb"))
-        total_score = 0
-        setHere = set(words)
+        with open ("JSONs/" + name.lower (), "r") as f:
+            for line in f:
+                data = json.loads(line)
+        tfidf = vectorizer.fit_transform([user_tweets, data[1]["content"]])
+
+        #cosine similarity
+        similar_scores[name] = ((tfidf * tfidf.T).A)[0, 1]
+
+    return similar_scores
+
+
+def score_user(leader_name):
+    user_tweets = translateTweetsJson (leader_name, False, False, False, 25)[1]["content"]
+    word_scores = []
+    similar_word_scores = {}
+    for name in screen_names:
+
+        tfidf_vect = pickle.load (open("pickle/" + name.lower(), "rb"))
+
+        # print (user_tweets)
         # for unique_word in set(user_tweets.split(" ")):
-        for unique_word in setHere:
+        for unique_word in set(user_tweets.split(" ")):
 
             #is the word in idf dictionary?
-            # print (tfidf_vect.get_feature_names())
-            #print (unique_word)
             if unique_word in tfidf_vect.get_feature_names():
               #word importance weight
 
               idf_index = tfidf_vect.get_feature_names().index(unique_word)
               idf_score = tfidf_vect.idf_[idf_index]
               word_score = user_tweets.count(unique_word) * idf_score
-              total_score += word_score
               word_scores.append((word_score,unique_word))
-        myScore = total_score/len(words)
-        minimum = min(minimum, myScore)
-        maximum = max(maximum, myScore)
-        sum += float(myScore)
-        scores[name] = myScore
-##        print(name+" DIVIDED = " + str(total_score/len(words)))
 
-    sc = [scores[word] for word in scores]
+        similar_word_scores[name] = word_scores
 
-    m = min(sc)
-    ran = max(sc) - m
-    av = sum(sc)/len(sc)
-
-    for name in scores:
-        scores[name] = (scores[name]-m)/ran
-
-    print(scores)
-
-    return word_scores
+    return similar_word_scores
 
 
-##compute_idf()
+# def score_user(screen_name):
+#     user_tweets = translateTweetsJson(screen_name, False, False, False, 25)[1]["content"]
+#     words = user_tweets.split(" ")
+#     #words = words[:500]
+#     print("NUM WORDS = " + str(len(words)))
+#     # print(user_tweets)
+#     word_scores = []
+#     scores = {}
+#     minimum = float("inf")
+#     maximum = float("inf")
+#     sum = float(0)
+#     for name in screen_names:
+#         tfidf_vect = pickle.load (open("pickle/"+name.lower(), "rb"))
+#         total_score = 0
+#         setHere = set(words)
+#         # for unique_word in set(user_tweets.split(" ")):
+#         for unique_word in setHere:
+#
+#             #is the word in idf dictionary?
+#             # print (tfidf_vect.get_feature_names())
+#             #print (unique_word)
+#             if unique_word in tfidf_vect.get_feature_names():
+#               #word importance weight
+#
+#               idf_index = tfidf_vect.get_feature_names().index(unique_word)
+#               idf_score = tfidf_vect.idf_[idf_index]
+#               word_score = user_tweets.count(unique_word) * idf_score
+#               total_score += word_score
+#               word_scores.append((word_score,unique_word))
+#         myScore = total_score/len(words)
+#         minimum = min(minimum, myScore)
+#         maximum = max(maximum, myScore)
+#         sum += float(myScore)
+#         scores[name] = myScore
+# ##        print(name+" DIVIDED = " + str(total_score/len(words)))
+#
+#     sc = [scores[word] for word in scores]
+#     print (sc)
+#     m = min(sc)
+#     ran = max(sc) - m
+#     av = sum(sc)/float(len(sc))
+#
+#     for name in scores:
+#         scores[name] = (scores[name]-m)/ran
+#
+#     print(scores)
+#
+#     return word_scores
+
+
+# compute_idf()
 print(score_user("jc_varela"))
 # print(score_user("realdonaldtrump"))
-# print(score_user("gudlaugurthor"))
-
-# translateTweetsJson("realdonalTrump", False, False, False, 25)[1]["content"]
 
 
 
