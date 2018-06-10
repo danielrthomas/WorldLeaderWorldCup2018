df593cea75945814e16e4a783d96322e256f6343
danielrthomas <danielrthomas1@gmail.com>
2018-06-10 13:06:24 -0500

    test

--- world_cup_flask.py
+++ world_cup_flask.py
@@ -1,6 +1,9 @@
 import json
 from flask import Flask,render_template_string,request,render_template
-app = Flask(__name__)                         
+from scoring import match_handle, compute_idf
+app = Flask(__name__)
+
+compute_idf()
 
 @app.route('/')
 def main():
@@ -10,7 +13,10 @@ def main():
         return render_template('input_handle.html')
     else:
         #we have username, now compute the scores and return
-        ranking = [[25,'Country','realDonaldTrump',['nasty','woman','im','with','her']]]
+        try:
+            ranking = match_handle(handle)
+        except:
+            ranking = []
         return render_template('result.html',rank=ranking)
 
 app.run()
