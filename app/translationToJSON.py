0dbe0dcc1051d568291b08c89ac59dcb9f6c746e
cohnj <cohnj@carleton.edu>
2018-06-12 17:09:36 -0500

    errors again

--- app/translationToJSON.py
+++ app/translationToJSON.py
@@ -51,6 +51,7 @@ def translation(line):
 
 def process_names(tokens):
     global f
+    f.write("process_names\n")
     here = nltk.ne_chunk(nltk.pos_tag(tokens))
     f.write(here)
     sent = ''
@@ -89,17 +90,18 @@ def checkCountry(word):
 def process(chunk):
     global translate
     global f
-    try:
-        f.write("process1\n")
-        return process_names(checkForEnglish(translation(chunk))) + " "
-    except:
-        try:
-            translate = Translator()
-            f.write("process2\n")
-            return process_names(checkForEnglish(translation(chunk))) + " "
-        except:
-            f.write("process3\n")
-            return ""
+    return process_names(checkForEnglish(translation(chunk))) + " "
+    # try:
+    #     f.write("process1\n")
+    #     return process_names(checkForEnglish(translation(chunk))) + " "
+    # except:
+    #     try:
+    #         translate = Translator()
+    #         f.write("process2\n")
+    #         return process_names(checkForEnglish(translation(chunk))) + " "
+    #     except:
+    #         f.write("process3\n")
+    #         return ""
 
 
 def translateTweetsJson(screen_name, include_retweets=False, saveTweets=False, saveTranslation=False, quant=4000):
