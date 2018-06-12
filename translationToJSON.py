cea3882c9ff94ea10fa929744aee1f59baf8e426
Olivia Cole <lulucole@dhcp-10-105-41-72.wireless.northwestern.private>
2018-06-12 14:15:36 -0500

    new translation

--- translationToJSON.py
+++ translationToJSON.py
@@ -12,12 +12,17 @@ dictionary = None
 countryDict = None
 translate = None
 
+remove = ['est', 'sous', 'nouveau', 'drogues', 'aux', 'ans', 'ill']
+
 def remove_emojis(line):
-    emoji_pattern = re.compile(u"([\U0001F600-\U0001F64F]|[\U0001F300-\U0001F5FF]|[\U0001F680-\U0001F6FF]|[\U0001F1E0-\U0001F1FF])+", flags=re.UNICODE)
-                                # emoticons  # symbols & pictographs
-                                 # transport & map symbols
-                                 # flags (iOS),
+    emoji_pattern = re.compile("["
+                               u"\U0001F600-\U0001F64F"  # emoticons
+                               u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+                               u"\U0001F680-\U0001F6FF"  # transport & map symbols
+                               u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+                               "]+", flags=re.UNICODE)
     return emoji_pattern.sub(r'', line)  # no emoji
+
 
 def splitElimination(line):
     words = line.split()
@@ -29,11 +34,13 @@ def splitElimination(line):
         elif word != '' and 'http' not in word and word[0] != '@':
             newline.append(word)
 
-    return ' '.join(newline)
+return ' '.join(newline)
+
 
 def translation(line):
     here = splitElimination(line)
     return (translate.translate(here)).text
+
 
 def process_names(tokens):
     here = nltk.ne_chunk(nltk.pos_tag(tokens))
@@ -43,25 +50,28 @@ def process_names(tokens):
             sent = sent + " " + x[0]
     return sent
 
+
 def checkForEnglish(line):
     global dictionary
+    global remove
     if dictionary == None:
         dictionary = enchant.Dict("en_US")
     removePunc = "".join((char for char in line if char not in string.punctuation))
     tokens = removePunc.split(" ")
     newline = []
     for word in tokens:
-        if len(word) > 0 and dictionary.check(word.lower()) and not checkCountry(word):
+        if ((len(word) == 1 and word.lower() in ['i','a']) or (len(word) > 2 and word != 'amp' and dictionary.check(word.lower()) and not checkCountry(word))) and word.lower() not in remove:
             newline.append(word)
 
-    return newline
+return newline
+
 
 def checkCountry(word):
     return word.lower() in countryDict
 
+
 def process(chunk):
     global translate
-    # print (process_names(checkForEnglish(translation(chunk))))
     try:
         return process_names(checkForEnglish(translation(chunk))) + " "
     except:
@@ -71,18 +81,19 @@ def process(chunk):
         except:
             return ""
 
+
 def translateTweetsJson(screen_name, include_retweets=False, saveTweets=False, saveTranslation=False, quant=4000):
     global translate
     global countryDict
     if translate == None:
         translate = Translator()
-
+    
     if countryDict == None:
         countryDict = {}
         for country in pycountry.countries:
             countryDict[country.name.lower()] = 0
 
-    tweetDict = get_all_tweets(screen_name, include_retweets, save=saveTweets, dict_output=True, quant=quant)
+tweetDict = get_all_tweets(screen_name, include_retweets, save=saveTweets, dict_output=True, quant=quant)
     content = (tweetDict[1]["content"]).split(" ")
     output = ""
     chunk = ""
@@ -90,25 +101,24 @@ def translateTweetsJson(screen_name, include_retweets=False, saveTweets=False, s
         if sys.getsizeof(chunk) > 1000:
             output += (re.sub(' +', ' ', process(chunk))).lower()
             chunk = ""
-
+        
         if sys.getsizeof(word) < 1000:
             chunk += word + " "
 
-    output += process(chunk)
+output += (re.sub(' +', ' ', process(chunk))).lower()
     tweetDict[1]["content"] = output
-
+    
     if saveTranslation:
-        with open("JSONs/" + screen_name.lower()+'.json', "w") as f:
+        with open("JSONs/" + screen_name.lower(), "w") as f:
             data = json.dumps(tweetDict)
             f.write(data)
 
-    return tweetDict
+return tweetDict
 
 
 # for s in screen_names:
 #     print(s)
 #     translateTweetsJson(s, False, False, True)
 #     print(s,'done')
-# print("sigmargabriel")
-# translateTweetsJson("sigmargabriel", False, False, True)
-# print('done')
+
+#translateTweetsJson('realdonaldtrump',False,False,False,25)
