e070baa5a4e0a5fdefa72fda06690ebd8dad07da
cohnj <cohnj@carleton.edu>
2018-06-12 18:24:57 -0500

    changing flask file

--- app/WorldCup.py
+++ app/WorldCup.py
@@ -13,17 +13,18 @@ except:
 @app.route('/')
 def main():
     return render_template('input_handle.html')
-    handle = request.args.get('handle')
-    if handle == None or handle == '':
-        #get username
-        return render_template('input_handle.html')
-    else:
-        #we have username, now compute the scores and return
-        try:
-            ranking = match_handle(handle,vects)
-        except:
-            ranking = []
-        return render_template('result.html',rank=ranking)
+# def main():
+#     handle = request.args.get('handle')
+#     if handle == None or handle == '':
+#         #get username
+#         return render_template('input_handle.html')
+#     else:
+#         #we have username, now compute the scores and return
+#         try:
+#             ranking = match_handle(handle,vects)
+#         except:
+#             ranking = []
+#         return render_template('result.html',rank=ranking)
 
 @app.route('/contact')
 def contact():
