557f661b6129b0541a7584ab5ca73c2af925728b
cohnj <cohnj@carleton.edu>
2018-06-12 19:31:54 -0500

    changing process_names()

--- app/translationToJSON.py
+++ app/translationToJSON.py
@@ -59,7 +59,7 @@ def process_names(tokens):
     #     if not hasattr(x, 'label') and not isinstance(x, nltk.Tree):
     #         sent = sent + " " + x[0]
     for x in tokens:
-        sent = sent + " " + x[0]
+        sent = sent + " " + x
     f.write("process_names\n")
     f.write("SENT: " + sent)
     return sent
